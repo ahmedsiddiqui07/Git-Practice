b3432999e44ac326273070755956f61114ace812
usmanirshad-NGI <usman.irshad@nextgeni.com>
2025-10-27 18:48:35 +0500

    create-function

---
+++ src/controllers/profile.js
@@ -0,0 +1,9 @@
+function usman() {
+    console.log("This is usman function")
+    return true
+}
+
+function shared() {
+    console.log("This is a shared function and all my teammates will write in it")
+    return 'Shared'
+}
