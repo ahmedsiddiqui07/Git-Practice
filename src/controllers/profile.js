2f7c7314b414d1d620061454bbc9b69a93b04058
ahmedsiddiqui07 <ahmed.siddiqui@nextgeni.com>
2025-10-27 16:45:44 +0500

    Added two function

---
+++ src/controllers/profile.js
@@ -0,0 +1,10 @@
+
+function ahmed(){
+    console.log("This is ahmed's function")
+    return 'Ahmed'
+}
+
+function shared(){
+    console.log("This is a shared function and all my teammates will write in it")
+    return 'Shared'
+}
